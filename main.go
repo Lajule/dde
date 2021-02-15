dd676b8a1e163b954ab823912961c361326ac44d
Julien ROUZIERES <julien.rouzieres@bjtmail.com>
2021-02-15 22:32:48 +0100

    temporary code

--- main.go
+++ main.go
@@ -6,8 +6,7 @@ import (
 	"github.com/webview/webview"
 )
 
-const (
-	CSS = `
+/*
 html,
 body {
   height: 100%;
@@ -39,68 +38,93 @@ body {
 }
 .bar {
   display: flex;
+  flex-direction: row-reverse;
   height: 40px;
-}`
-)
+}
+*/
 
 // Version Program version
 var Version = "development"
 
 func main() {
-	w := webview.New(false)
+	w := webview.New(true)
 	defer w.Destroy()
 
 	w.SetTitle(fmt.Sprintf("dde %s", Version))
 	w.SetSize(800, 600, webview.HintNone)
 
-	w.Bind("css", func() string {
-		return CSS
+	w.Bind("save", func() {
 	})
 
 	w.Bind("quit", func() {
 		w.Terminate()
 	})
 
+	w.Eval(`
+    window.addEventListener('DOMContentLoaded', event => {
+      var btns = document.querySelectorAll('.add')
+      btns.forEach(btn => {
+        btn.addEventListener('click', event => {
+          section = btn.parentNode
+          var task = section.getElementsByTagName('input')[0]
+          if (task.value != '') {
+            var div = document.createElement('div')
+            var input = document.createElement('input')
+            input.type = 'checkbox'
+            div.appendChild(input)
+            var label = document.createElement('label')
+            label.innerHTML = task.value
+            div.appendChild(label)
+            section.appendChild(div)
+            task.value = ''
+          }
+        })
+      })
+      clear = document.getElementById('clear')
+      clear.addEventListener('click', event => {
+        boxes = document.querySelectorAll('input[type=checkbox]')
+      })
+    })
+`)
+
 	w.Navigate(`data:text/html,
 <!doctype html>
 <html>
-  <body>
-    <div class="matrix">
-      <div id="do">
-        <h1>Do</h1>
-        <h5>Important/Urgent</h5>
-        <div>
-          <input type="checkbox" id="do-0" checked>
-          <label for="do-0">Do something in Golang</label>
-        </div>
-      </div>
-      <div id="schedule">
-        <h1>Schedule</h1>
-        <h5>Important/Not Urgent</h5>
-      </div>
-      <div id="delegate">
-        <h1>Delegate</h1>
-        <h5>Not Important/Urgent</h5>
-      </div>
-      <div id="cancel">
-        <h1>Cancel</h1>
-        <h5>Not Important/Not Urgent</h5>
-      </div>
+  <div class=matrix>
+    <div id=do>
+      <h1>Do</h1>
+      <h5>Important/Urgent</h5>
+      <button class=add>Add</button>
+      <input type=text>
+      <div class=tasks></div>
     </div>
-    <div class="bar">
-      <button onclick="quit();">Quit</button>
+    <div id=schedule>
+      <h1>Schedule</h1>
+      <h5>Important/Not Urgent</h5>
+      <button class=add>Add</button>
+      <input type=text>
+      <div class=tasks></div>
     </div>
-    <script>
-      window.onload = function () {
-        var ref = document.querySelector('script');
-        css().then(function (code) {
-          var el = document.createElement('style');
-          el.innerHTML = code;
-          ref.parentNode.insertBefore(el, ref);
-        });
-      };
-    </script>
-  </body>
+    <div id=delegate>
+      <h1>Delegate</h1>
+      <h5>Not Important/Urgent</h5>
+      <button class=add>Add</button>
+      <input type=text>
+      <div class=tasks></div>
+    </div>
+    <div id=cancel>
+      <h1>Cancel</h1>
+      <h5>Not Important/Not Urgent</h5>
+      <button class=add>Add</button>
+      <input type=text>
+      <div class=tasks></div>
+    </div>
+  </div>
+  <div class=bar>
+    <button id=quit>Quit</button>
+    <button id=save>Save</button>
+    <button id=clear>Clear</button>
+  </div>
 </html>`)
 
 	w.Run()
